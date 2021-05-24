bafd8cca8f2409e261287bf9213b4c0819aa8e3f
heronet <sirat4757@gmail.com>
2021-05-25 00:30:21 +0600

    Misc Changes

---
+++ Migrations/20210524182708_Fixed spelling mistakes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mcqbk.Migrations
+{
+    public partial class Fixedspellingmistakes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
