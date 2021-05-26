857f81b445c17d256ef997123e002650417b0b74
heronet <sirat4757@gmail.com>
2021-05-27 00:03:23 +0600

    Removed admin controller protection

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -10,7 +10,7 @@ using Services;
 
 namespace Controllers
 {
-    [Authorize(Roles = "Admin")]
+    // [Authorize(Roles = "Admin")] 
     public class AdminController : DefaultController
     {
         private readonly UserManager<EntityUser> _userManager;
