0c715cc3948b2fd613acfddbbd3bf6a368283c5d
heronet <sirat4757@gmail.com>
2021-05-27 20:25:58 +0600

    Added Database selection on startup

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -36,21 +36,34 @@ namespace Extensions
                 }
                 else
                 {
-                    // Use connection string provided at runtime by Heroku.
+                    // Use connection string provided at runtime.
                     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    var host = Environment.GetEnvironmentVariable("HostName");
+                    if (host == "Heroku") // Use Heroku Postgres
+                    {
+                        // Parse connection URL to connection string for Npgsql
+                        connUrl = connUrl.Replace("postgres://", string.Empty);
+                        var pgUserPass = connUrl.Split("@")[0];
+                        var pgHostPortDb = connUrl.Split("@")[1];
+                        var pgHostPort = pgHostPortDb.Split("/")[0];
+                        var pgDb = pgHostPortDb.Split("/")[1];
+                        var pgUser = pgUserPass.Split(":")[0];
+                        var pgPass = pgUserPass.Split(":")[1];
+                        var pgHost = pgHostPort.Split(":")[0];
+                        var pgPort = pgHostPort.Split(":")[1];
 
-                    // Parse connection URL to connection string for Npgsql
-                    connUrl = connUrl.Replace("postgres://", string.Empty);
-                    var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@")[1];
-                    var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("/")[1];
-                    var pgUser = pgUserPass.Split(":")[0];
-                    var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split(":")[0];
-                    var pgPort = pgHostPort.Split(":")[1];
-
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true";
+                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true";
+                    }
+                    else // Use ElephantSQL Postgres
+                    {
+                        var uriString = connUrl;
+                        var uri = new Uri(uriString);
+                        var db = uri.AbsolutePath.Trim('/');
+                        var user = uri.UserInfo.Split(':')[0];
+                        var passwd = uri.UserInfo.Split(':')[1];
+                        var port = uri.Port > 0 ? uri.Port : 5432;
+                        connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}", uri.Host, db, user, passwd, port);
+                    }
                 }
 
                 // Whether the connection string came from the local development configuration file
